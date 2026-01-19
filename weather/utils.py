fb8e0ffcba81834e0c83bf555016634ad44db79a
Republic Oba <republicoba1@gmail.com>
2026-01-20 00:40:40 +0100

    setup and backend configuration with startup on code

---
+++ weather/utils.py
@@ -0,0 +1,24 @@
+import requests
+from django.conf import settings
+from django.core.cache import cache
+
+def get_weather_app(city):
+    cache_key = f'weather_{city}'
+    cached_data = cache.get(cache_key)
+    if cached_data:
+        return cached_data
+    api_key = settings.OPENWEATHER_API_KEY
+    base_url = "https://api.openweathermap.org/data/2.5/weather"
+    data = {
+        'city': city,
+        'appid': api_key,
+        'units': 'metric',
+    }
+    try:
+        response = requests.get(base_url, params=data)
+        response.raise_for_status()
+        data = response.json()
+        cache.set(cache_key, 600)
+        return data
+    except requests.exceptions.RequestException as e:
+        raise None
